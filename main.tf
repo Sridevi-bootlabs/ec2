dfd67052cda3117018965acf16ee46819c51fcf4
Sridevi-bootlabs <105430846+Sridevi-bootlabs@users.noreply.github.com>
2024-02-12 13:37:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,8 @@
-resource "aws_instance" "web-server" {
-  ami                         = "ami-0557a15b87f6559cf"
-  instance_type               = "t2.micro"
-  associate_public_ip_address = true
-  key_name                    = "sailorcostest"
-  subnet_id                   = "subnet-0c74fdd30bc2a6487"
-  root_block_device {
-    volume_size           = 30
-  }
-  metadata_options {
-    http_endpoint = "enabled"
-  }
+resource "aws_instance" "web" {
+  ami           = "ami-0557a15b87f6559cf"
+  instance_type = "t2.micro"
+
   tags = {
     Name = "env0test"
   }
-
 }
